4dc63264c923bd9931a7b319ac39429f4cfd4214
avi@robusta.dev <avi@robusta.dev>
2023-10-01 17:52:56 +0300

    added playbook for kube version mismatch

--- playbooks/robusta_playbooks/version_mismatch_enricher.py
+++ playbooks/robusta_playbooks/version_mismatch_enricher.py
@@ -1,7 +1,14 @@
 import logging
 
-from prometrix import PrometheusQueryResult
-from robusta.api import PROMETHEUS_REQUEST_TIMEOUT_SECONDS, ExecutionBaseEvent, PrometheusParams, action
+from robusta.api import (
+    PROMETHEUS_REQUEST_TIMEOUT_SECONDS,
+    ExecutionBaseEvent,
+    MarkdownBlock,
+    PrometheusParams,
+    SlackAnnotations,
+    TableBlock,
+    action,
+)
 from robusta.integrations.prometheus.utils import get_prometheus_connect
 
 
@@ -16,13 +23,41 @@ def version_mismatch_enricher(event: ExecutionBaseEvent, params: PrometheusParam
 
     try:
         prom = get_prometheus_connect(params)
-        query = 'count by (git_version, cluster, node) (label_replace(kubernetes_build_info{job!~"kube-dns|coredns"}, "git_version", "$1", "git_version", "(v[0-9]*.[0-9]*).*"))'
+        list_version_query = 'count by (git_version, cluster, node) (label_replace(kubernetes_build_info{job!~"kube-dns|coredns"}, "git_version", "$1", "git_version", "(v[0-9]*.[0-9]*).*"))'
         prom_params = {"timeout": PROMETHEUS_REQUEST_TIMEOUT_SECONDS}
         prom.check_prometheus_connection(prom_params)
-        result = prom.custom_query(query=query, params=prom_params)
-        logging.warning(result)
+        results = prom.custom_query(query=list_version_query, params=prom_params)
+        kubernetes_api_version = max(
+            [
+                result.get("metric", {}).get("git_version")
+                for result in results
+                if result.get("metric", {}).get("node") is None
+            ]
+        )
+        nodes_by_version = [
+            [result.get("metric", {}).get("node"), result.get("metric", {}).get("git_version")]
+            for result in results
+            if result.get("metric", {}).get("node") is not None
+        ]
+        logging.warning(results)
+        logging.warning(kubernetes_api_version)
+        logging.warning(nodes_by_version)
         # event.add_enrichment(
         #    [PrometheusBlock(data=prometheus_result, query=params.promql_query)],
         # )
+        event.add_enrichment(
+            [
+                MarkdownBlock(f"The kubernetes api server is version {kubernetes_api_version}."),
+                TableBlock(
+                    nodes_by_version,
+                    ["name", "version"],
+                    table_name="*Node Versions*",
+                ),
+                MarkdownBlock(
+                    f"To solver this alert, make sure to update all of your nodes to version {kubernetes_api_version}."
+                ),
+            ],
+            annotations={SlackAnnotations.ATTACHMENT: True},
+        )
     except Exception:
         logging.error(f"Failed getting query", exc_info=True)
